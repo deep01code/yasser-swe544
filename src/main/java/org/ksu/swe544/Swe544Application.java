68e314275cb763c71c5f0eb9e55289f022bb26e6
yasser <deep01code@gmail.com>
2024-11-20 13:55:07 +0300

    first commit

---
+++ src/main/java/org/ksu/swe544/Swe544Application.java
@@ -0,0 +1,13 @@
+package org.ksu.swe544;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swe544Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swe544Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/ksu/swe544/Swe544ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ksu.swe544;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swe544ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
